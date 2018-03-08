f6070e919d1e4d39a4e76e898e8ef36f073a04d1
Md. Hafizur Rahman Arfin <arfin97@gmail.com>
2018-03-09 03:43:59 +0630

    Fixed Restart Bug
    There was a problem while the re start button was pressed. The game board woudn't fully reset due to a loop limit. It's fixed now.

--- js/script.js
+++ js/script.js
@@ -15,8 +15,8 @@ var board = [
 ]
 
 $("#reset").click(function () {
-	for(var i = 0; i < 5; i++){
-		for(var j = 0; j < 6; j++){
+	for(var i = 0; i < 7; i++){
+		for(var j = 0; j < 7; j++){
 			board[i][j] = 0;
 		} 
 	}
